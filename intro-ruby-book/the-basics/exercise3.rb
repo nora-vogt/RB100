5557ee2c3eda4a007ae3cfd04910d611950cbca2
Nora Vogt <noravogt@protonmail.com>
2022-12-29 13:35:27 -0500

    first attempt at exercise 3

---
+++ intro-ruby-book/the-basics/exercise3.rb
@@ -0,0 +1,10 @@
+movies = {
+  :"The X-Files" => 1998,
+  :"2001 A Space Odyssey" => 1968,
+  :"The Sound of Music" => 1965,
+  :"Coffee and Cigarettes" => 2003
+}
+
+movies.each do |k, v|
+  puts v
+end
