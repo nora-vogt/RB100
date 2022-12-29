9b0cc6de2ff827ee653de25c8f637d0eb3b4eb4b
Nora Vogt <noravogt@protonmail.com>
2022-12-29 13:08:23 -0500

    completed exercise 2

---
+++ intro-ruby-book/the-basics/exercise2.rb
@@ -0,0 +1,9 @@
+thousands = 7341 / 1000
+hundreds = 7341 % 1000 / 100
+tens = 7341 % 100 / 10
+ones = 7341 % 10
+
+puts "thousands: #{thousands}"
+puts "hundreds: #{hundreds}"
+puts "tens: #{tens}"
+puts "ones: #{ones}"
