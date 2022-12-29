81ecf9cd0dc30d2683650ccb43dc4a4e2e05f037
Nora Vogt <noravogt@protonmail.com>
2022-12-29 13:39:35 -0500

    refactored exercise 3

--- intro-ruby-book/the-basics/exercise3.rb
+++ intro-ruby-book/the-basics/exercise3.rb
@@ -1,3 +1,18 @@
+movies = { :x_files => 1998,
+           :a_space_odyssey_2001 => 1968,
+           :the_sound_of_music => 1965,
+           :coffee_and_cigarettes => 2003
+}
+
+puts movies[:x_files]
+puts movies[:a_space_odyssey_2001]
+puts movies[:the_sound_of_music]
+puts movies[:coffee_and_cigarettes]
+
+
+=begin
+original solution
+
 movies = {
   :"The X-Files" => 1998,
   :"2001 A Space Odyssey" => 1968,
@@ -8,3 +23,4 @@ movies = {
 movies.each do |k, v|
   puts v
 end
+=end
