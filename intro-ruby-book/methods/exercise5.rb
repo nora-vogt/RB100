52d240e4e3d730f4a053c13f2af2e3774d96c0eb
Nora Vogt <noravogt@protonmail.com>
2023-01-02 11:43:55 -0500

    completed exercise 5

---
+++ intro-ruby-book/methods/exercise5.rb
@@ -0,0 +1,10 @@
+# Edit code from exercise 4 so that it will print words to the screen.
+
+def scream(words)
+  words = words + "!!!!"
+  puts words
+end
+
+scream("Yippeee")
+
+# The method will now print the output of the scream method to the screen, but the return value will still be nil.
