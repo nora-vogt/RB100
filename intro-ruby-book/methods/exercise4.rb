0af6a2dce4ccbfc5372ae2f65de7071461a6e9cb
Nora Vogt <noravogt@protonmail.com>
2023-01-02 14:20:27 -0500

    added comment to exercise 4

--- intro-ruby-book/methods/exercise4.rb
+++ intro-ruby-book/methods/exercise4.rb
@@ -8,4 +8,4 @@ end
 
 scream("Yippeee")
 
-# This program will not print anything, because the method will return on line 5 (exit the method) before executing the puts. The method's return value will be nil
+# This program will not print anything, because the method will return on line 5 (exit the method) before executing the puts. The method's return value will be nil.
