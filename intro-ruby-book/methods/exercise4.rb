97c443f5f47a844507b8452fd31fa6d0e4678882
Nora Vogt <noravogt@protonmail.com>
2023-01-02 11:39:31 -0500

    completed exercise 4

---
+++ intro-ruby-book/methods/exercise4.rb
@@ -0,0 +1,11 @@
+# What will the following code print to the screen?
+
+def scream(words)
+  words = words + "!!!!"
+  return
+  puts words
+end
+
+scream("Yippeee")
+
+# This program will not print anything, because the method will return on line 5 (exit the method) before executing the puts. The method's return value will be nil
