c2197b256ba39b9000cb4662b36d3e9a0d922ba2
Nora Vogt <noravogt@protonmail.com>
2023-02-15 09:32:03 -0500

    refactored exercise 3

--- intro-ruby-book/loops-iterators/exercise3.rb
+++ intro-ruby-book/loops-iterators/exercise3.rb
@@ -1,30 +1,14 @@
 # Write a method that counts down to zero using recursion.
 
-# Refactoring
 def countdown(number)
-  if number > 0
+  if number <= 0
     puts number
-    countdown(number - 1)
   else
     puts number
+    countdown(number - 1)
   end
 end
 
 countdown(10)
-
-# First Attempt
-
-=begin
-def countdown(number)
-  if number > 0
-    puts number
-    number -= 1
-    countdown(number)
-  else
-    puts number
-  end
-end 
-=end
-
   
 
