84e89ce03566975b5cf78b9c59be6e70e4d69f89
Nora Vogt <noravogt@protonmail.com>
2023-02-23 18:10:14 -0500

    added lesson files

---
+++ intro-ruby-book/methods/say.rb
@@ -0,0 +1,10 @@
+def say(words='hello')  # 'words' is the parameter
+  puts words + '.'
+end
+
+say()
+say("hi")
+say("how are you")
+say("I'm fine")
+
+# This method extracts the logic of printing out text, giving us more flexibility in our program. 
