54602197fb2fa300f30ccf3997587ae94cb7ba26
Nora Vogt <noravogt@protonmail.com>
2023-02-23 15:17:46 -0500

    completed exercise 7

---
+++ intro-ruby-book/the-basics/exercise7.rb
@@ -0,0 +1,6 @@
+# What does the following error message tell you?
+
+# SyntaxError: (irb):2: syntax error, unexpected ')', expecting '}'
+ #  from /usr/local/rvm/rubies/ruby-2.5.3/bin/irb:16:in `<main>'
+
+# On line 2, a closed parenthesis has been provided instead of a closed curly bracket '}'. This is needed to close something like a hash.
