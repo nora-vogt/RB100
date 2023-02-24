ec9c5732b12709db44b968f88f4d958cf72d38a7
Nora Vogt <noravogt@protonmail.com>
2023-02-24 12:41:18 -0500

    adding deleted flow control lesson files

---
+++ intro-ruby-book/flow-control/examples.rb
@@ -0,0 +1,27 @@
+# Example 1
+if x == 3
+  puts "x is 3"
+end
+
+# Alt Example 1
+puts "x is 3" if x == 3
+
+# Example 2
+if x == 3
+  puts "x is 3"
+elsif x == 4
+  puts "x is 4"
+end
+
+# Example 3
+if x == 3
+  puts "x is 3"
+else
+  puts "x is NOT 3"
+end
+
+# Example 4: must use "then" keyword when using 1-line syntax
+if x == 3 then puts "x is 3" end
+
+# Unless Example
+puts "x is NOT 3" unless x == 3
---
+++ intro-ruby-book/flow-control/ternary_example.rb
@@ -0,0 +1,5 @@
+true ? "this is true" : "this is not true"
+# => "this is true"
+
+false ? "this is true" : "this is not true"
+# => "this is not true"
