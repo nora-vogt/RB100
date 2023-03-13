f61737af66fe64bb22a728d439cc02823f9df8b9
Nora Vogt <noravogt@protonmail.com>
2023-03-13 12:15:41 -0400

    added flow control for non-numeric values

--- exercises/user-input/your-age-in-months.rb
+++ exercises/user-input/your-age-in-months.rb
@@ -1,4 +1,15 @@
 puts ">> What is your age in years?"
-age = gets.chomp.to_i
+age_in_years = ''
 
-puts "You are #{age * 12} months old."
+loop do 
+  age_in_years = gets.chomp.to_i
+  if age_in_years > 0
+    break
+  else
+    puts "Please enter your age as a positive number:"
+  end
+end
+
+age_in_months = age_in_years * 12
+
+puts "You are #{age_in_months} months old."
