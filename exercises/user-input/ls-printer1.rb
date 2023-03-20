c628b991c21cededf72dbadbe902e493fbf101be
Nora Vogt <noravogt@protonmail.com>
2023-03-20 12:29:29 -0400

    refactored exercise

--- exercises/user-input/ls-printer1.rb
+++ exercises/user-input/ls-printer1.rb
@@ -1,17 +1,11 @@
-# Write a program that prints 'Launch School is the best!' repeatedly UNTIL x number of lines have been printed
-# the number of lines (x) should be obtained from the user
-# instructions must include that at least 3 lines are printed
-# use #to_i to convert user input to an Integer
-
 number_of_lines = nil
 
 loop do
   puts ">> How many output lines do you want? Enter a number >= 3:"
-  number_of_lines = gets.chomp.to_i
-  
+  number_of_lines = gets.to_i  # don't need #chomp when converting to an Integer
   break if number_of_lines >= 3
-  
+
   puts ">> That's not enough lines."
 end
 
-number_of_lines.times { puts "Launch School is the best!"}
+number_of_lines.times { puts "Launch School is the best!" }
