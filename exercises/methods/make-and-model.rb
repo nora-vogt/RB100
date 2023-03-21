dabf1929b2b13c66aeba288a4e0b9db8498be61c
Nora Vogt <noravogt@protonmail.com>
2023-03-21 09:54:13 -0400

    completed exercise

---
+++ exercises/methods/make-and-model.rb
@@ -0,0 +1,9 @@
+# write method 'car'
+  # car takes 2 arguments
+  # prints a string containing the values of both arguments
+
+def car(make, model)
+  puts "#{make} #{model}"
+end
+
+car('Toyota', 'Corolla')
