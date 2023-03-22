705e871a40556dc864d3d53ac29d59e43674f4ae
Nora Vogt <noravogt@protonmail.com>
2023-03-22 12:24:50 -0400

    added notes

--- exercises/conditionals/cool-numbers.rb
+++ exercises/conditionals/cool-numbers.rb
@@ -1,6 +1,6 @@
 number = rand(10)
 
-if number == 5   # this was variable assignment in original code, not comparison
+if number == 5   # this was variable assignment in original code (number = 5), not comparison. var. assignment evaluates to true, so this condition will always evalaute as true. 
   puts '5 is a cool number!'
 else
   puts 'Other numbers are cool too!'
