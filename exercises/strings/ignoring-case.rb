8df48062ac92b8a2ad6aadab98243a5a06a6c2da
Nora Vogt <noravogt@protonmail.com>
2023-03-25 12:43:01 -0400

    added more notes

--- exercises/strings/ignoring-case.rb
+++ exercises/strings/ignoring-case.rb
@@ -6,10 +6,10 @@ puts name.casecmp?('DAVE')  # returns false
 
 #casecmp is a case-insensitive version of String#<=>, returns -1, 0, 1, or nil
 puts name.casecmp('RoGeR') == 0 
-# name.casecmp('RoGeR') returns 0, strings match when case insensitive
+# name.casecmp('RoGeR') returns 0, strings are equal when case-insensitive
 
 puts name.casecmp('DAVE') == 0
-# name.casecmp('DAVE') returns 1. the value of the calling string (name) is greater than the argument provided ('DAVE').
+# name.casecmp('DAVE') returns 1. the case-insensitive value of the calling string (name) is greater than the argument provided ('DAVE').
 
 puts name.casecmp('Zepplin') 
-# name.casecmp('Zepplin') returns -1. the value of the calling string name is less than the argument provided ('Zepplin'). 
+# name.casecmp('Zepplin') returns -1. the case-insensitive value of the calling string name is less than the argument provided ('Zepplin'). 
