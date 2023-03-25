5e51ef38e69b774aac2fbde4cdb02de81bd2e238
Nora Vogt <noravogt@protonmail.com>
2023-03-25 14:47:56 -0400

    refactored exercise

--- exercises/strings/are-you-there.rb
+++ exercises/strings/are-you-there.rb
@@ -1,14 +1,5 @@
 colors = 'blue pink yellow orange'
 
-if colors.include?('yellow')
-  puts 'true'
-else 
-  puts 'false'
-end
-
-if colors.include?('purple')
-  puts 'true'
-else 
-  puts 'false'
-end
-  
+# refactored. #include? returns a boolean, so we can output the return value without a conditional statement
+puts colors.include?('yellow')
+puts colors.include?('purple')
