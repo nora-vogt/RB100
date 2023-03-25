f1140ef503fcf194e5049222c85bf7d46c2ddcef
Nora Vogt <noravogt@protonmail.com>
2023-03-25 10:37:24 -0400

    fixed formatting, added notes

--- exercises/conditionals/stoplight3.rb
+++ exercises/conditionals/stoplight3.rb
@@ -1,7 +1,7 @@
 stoplight = ['green', 'yellow', 'red'].sample
 
 case stoplight
-when 'green' then puts 'Go!' 
+when 'green'  then puts 'Go!'   # make sure then statements are aligned
 when 'yellow' then puts 'Slow down!' 
-else puts 'Stop!'
+else               puts 'Stop!'   # else does not need then because no specific condition is provided to else
 end
