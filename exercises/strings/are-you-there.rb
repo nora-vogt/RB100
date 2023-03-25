9a27885a8f22aa0bf7b5ef7f9759f88f5b038759
Nora Vogt <noravogt@protonmail.com>
2023-03-25 14:46:42 -0400

    first try

---
+++ exercises/strings/are-you-there.rb
@@ -0,0 +1,14 @@
+colors = 'blue pink yellow orange'
+
+if colors.include?('yellow')
+  puts 'true'
+else 
+  puts 'false'
+end
+
+if colors.include?('purple')
+  puts 'true'
+else 
+  puts 'false'
+end
+  
