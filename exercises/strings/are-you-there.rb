112b395a4c0c59e3bfd375243fa708e47992f348
Nora Vogt <noravogt@protonmail.com>
2023-03-26 10:02:43 -0400

    updated notes

--- exercises/strings/are-you-there.rb
+++ exercises/strings/are-you-there.rb
@@ -1,5 +1,10 @@
 colors = 'blue pink yellow orange'
 
 # refactored. #include? returns a boolean, so we can output the return value without a conditional statement
-puts colors.include?('yellow')
-puts colors.include?('purple')
+puts colors.include?('yellow')  # true
+puts colors.include?('purple')  # false
+
+colors = 'blue boredom yellow'
+puts colors.include?('red')     # true
+
+# this will return true, because include? returns true if the calling string contains the argument string anywhere - not necessarily as its own word. in this case, 'red' is found within the calling string as part of the word 'boREDdom'.
