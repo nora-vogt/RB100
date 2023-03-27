7ef30f38a86535ad23e6d89fed54a566207bb271
Nora Vogt <noravogt@protonmail.com>
2023-03-27 13:33:51 -0400

    completed exercise

---
+++ exercises/hashes/adding-the-year.rb
@@ -0,0 +1,11 @@
+car = {
+  type: 'sedan',
+  color: 'blue',
+  mileage: 80_000
+}
+
+car[:year] = 2003
+
+#takeaway:
+  # use element assignment Hash[]= to add a new key value pair to a hash, or modify an existing value
+  # hash[key] = value
