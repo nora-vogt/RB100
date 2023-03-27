6cc9b3a61471691b418e2ff0fd56b3fb1d3bbd1a
Nora Vogt <noravogt@protonmail.com>
2023-03-27 11:38:22 -0400

    completed exercise

---
+++ exercises/arrays/are-we-the-same.rb
@@ -0,0 +1,5 @@
+array1 = [1, 5, 9]
+array2 = [1, 9, 5]
+
+puts array1 == array2
+# this will print false. arrays are ordered lists, so while these arrays contain the same numbers, the orders are different - making them different arrays. 
