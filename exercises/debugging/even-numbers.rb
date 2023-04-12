f5cf815f579394baebb1e57b4e2c658ca5da5f0b
Nora Vogt <noravogt@protonmail.com>
2023-04-12 10:07:47 -0500

    added notes

--- exercises/debugging/even-numbers.rb
+++ exercises/debugging/even-numbers.rb
@@ -1,9 +1,11 @@
 numbers = [5, 2, 9, 6, 3, 1, 8]
 
 even_numbers = numbers.select do |n|
-  n if n.even?   # The block is originally returning an integer n if n is even, and nil if n is odd. 
+  n.even?   # can simplify and remove the if statement. #even? returns a boolean, and the boolean will be the return value of the block.
 end
 
-# To fix this, we can use the #select method instead of #map. 
+# The block is originally returning an integer n if n is even, and nil if n is odd. 
+
+# To fix this, we can use the #select method instead of #map. #select returns a new array containing only the elements of the original array for which the return value of the block evaluates as true. 
 
 p even_numbers
