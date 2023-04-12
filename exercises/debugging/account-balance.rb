01f1d9010f7f2f82f034b71a3f8ab0a79227b042
Nora Vogt <noravogt@protonmail.com>
2023-04-12 10:29:00 -0500

    first try

---
+++ exercises/debugging/account-balance.rb
@@ -0,0 +1,37 @@
+# Financially, you started the year with a clean slate.
+
+balance = 0
+
+# Here's what you earned and spent during the first three months.
+
+january = {
+  income: [ 1200, 75 ],
+  expenses: [ 650, 140, 33.2, 100, 26.9, 78 ]
+}
+
+february = {
+  income: [ 1200 ],
+  expenses: [ 650, 140, 320, 46.7, 122.5 ]
+}
+
+march = {
+  income: [ 1200, 10, 75 ],
+  expenses: [ 650, 140, 350, 12, 59.9, 2.5 ]
+}
+
+# Let's see how much you've got now...
+
+def calculate_balance(month)
+  plus  = month[:income].sum
+  minus = month[:expenses].sum
+
+  plus - minus
+end
+
+[january, february, march].each do |month|
+  balance += calculate_balance(month)
+end
+
+puts balance # As originally written, the variable balance is being re-assigned during each iteration of the #each method called on line 31. This results in balance referencing to the result of calling the calculate_balance method on the march hash - about 70.
+
+# To fix this, we can use the "add and assign" operator on line 32, instead of assignment. This will add the result of calling calculate_balance on each month's hash to the variable balance.
