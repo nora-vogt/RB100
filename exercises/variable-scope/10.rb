1bf7454ad9fbf217cd300b9bf8f7a4b3771e4b11
Nora Vogt <noravogt@protonmail.com>
2023-04-14 11:27:46 -0500

    second pass completing exercise

--- exercises/variable-scope/10.rb
+++ exercises/variable-scope/10.rb
@@ -1,28 +1,13 @@
 a = 7
 array = [1, 2, 3]
 
-def my_value(ary)
-  ary.each do |b|
-    a += b
+def my_value(ary)   # initially when invoked ary references same Array object as local var array
+  ary.each do |b|   # b is a var local to the each block
+    a += b    # this line will cause an error; there is no a initialized within the method
   end
 end
 
-my_value(array)  # will throw an exception - undefined method '+' for nil:NilClass (NoMethodError). This is because the local var 'a' initialized on line 1 is not available to the invocation of #each with a block. Because the block is called within the method definition of my_value, it obeys the self-contained scope rules in regard to local variables. The outer 'a' is not visible inside the my_value method definition, so it isn't visible inside the method invocation with a block.
-puts a   # will not run
+my_value(array) # an error will be raised with this method call
+puts a
 
-
-a = 7
-array = [1, 2, 3]
-
-def my_value(ary, a)
-  ary.each do |b|
-    a += b
-  end
-end
-
-my_value(array, a)
-puts a # still prints 7. Inside the method, it's a new local var 'a' that points to the same value as the var initialized on line '1'. two different variables called 'a' pointing to the same Integer object.
-
-# line 6 is reassigning the variable 'a' local to the method to point at a different object (another Integer).
-
-# also, numbers are immutable in Ruby. it's impossible for us to mutate the value of 'a' initialized on line 1 from inside the method. 
+# Calling the my_value method on line 10 will raise an error. While the inner scope of blocks can access outer scope, this each block is located within the method body of the my_value method. Methods have self-contained scope, and the local variable a initialized on line 1 is not accessible from inside the method definition. Trying to reference a variable 'a' inside the block on line 6 will raise an error, because no variable named a has been initialized inside of the my_value method.
