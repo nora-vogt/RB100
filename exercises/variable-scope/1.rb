9988c3a913fb91e945e0fe1756d1965bdfdf0854
Nora Vogt <noravogt@protonmail.com>
2023-04-14 09:59:33 -0500

    second pass completing exercise

--- exercises/variable-scope/1.rb
+++ exercises/variable-scope/1.rb
@@ -1,10 +1,10 @@
 a = 7
 
 def my_value(b)
-  b += 10
+  b += 10 # variable reassignment: b = b + 10
 end
 
-my_value(a)
-puts a
+my_value(a) # method will return 17
+puts a  # will output 7
 
-# This will print 7. Calling the my_value method on like 7 will return 17, but the value of a is not modified inside the method body.
+# The #puts call on line 8 will print 7. The value associated with the variable a (7) is not changed by invoking the my_value method. Numbers are immutable in Ruby, and on line 4, the variable local to the method definition (b) is being reassigned to reference the integer 17.
