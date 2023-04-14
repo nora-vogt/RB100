ad45cd378e485cb6566560fe8a9f4c5227040394
Nora Vogt <noravogt@protonmail.com>
2023-04-14 11:09:58 -0500

    second pass completing exercise

--- exercises/variable-scope/9.rb
+++ exercises/variable-scope/9.rb
@@ -1,10 +1,12 @@
 a = 7
 array = [1, 2, 3]
 
-array.each do |a|
-  a += 1
+array.each do |a|   # this is a different var a, local to the block 
+  a += 1    # var reassignment; a = a + 1
 end
 
-puts a  # 7
+puts a  # will print 7
 
-# 7 will be output on line 8 due to variable shadowing. While iterating through the 'array' with #each, the local var |a| defined for the block is local to the block. The block doesn't know about the local var a initialized on line 1. The code inside the block doesn't do anything to modify the 'a' on line 1. 
+# This is an example of shadowing - there are two local variables with the same name. While the local variable initialized on line 1 can be accessed from within the block, the a on line 5 of the block is a different variable local to the block. It is that block local variable that is being reassigned by line 5. 
+
+# The #puts call on line 8 will output the value of the a initialized on line 1 - 7.
