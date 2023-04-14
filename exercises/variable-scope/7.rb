12cf1356abd7c90999f33f0a95d0121fa73353de
Nora Vogt <noravogt@protonmail.com>
2023-04-14 10:57:59 -0500

    second pass completing exercise

--- exercises/variable-scope/7.rb
+++ exercises/variable-scope/7.rb
@@ -2,12 +2,9 @@ a = 7
 array = [1, 2, 3]
 
 array.each do |element|
-  a = element
-end   # returns [1, 2, 3]
+  a = element   # on each iteration, a is being reassigned to the value of element 
+end
 
-puts a  # will print 3
+puts a  # prints 3
 
-# Blocks do have access to variables initialized outside of the block. Each time the #each method iterates through each element in the array, variable 'a' is reassigned to point to the Integer object bound to |element|. 
-
-# So iteration 1, a = 1; iteration 2 a = 2, iteration 3 a = 3. On line 8, puts a will print 3. 
-
+# The inner scope of a block has access to variables initialized in the outer scope, making the code on line 5 variable reassignment. With each iteration through the array, the variable a is reassigned to point to the value represented by 'element'. With the last iteration, a is reassigned to reference the Integer 3, so 3 will be printed by line 8.
