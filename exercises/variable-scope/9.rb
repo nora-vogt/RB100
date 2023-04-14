cf952331f45b334b1fbede090bcea809d6d2c307
Nora Vogt <noravogt@protonmail.com>
2023-04-14 11:15:38 -0500

    added notes

--- exercises/variable-scope/9.rb
+++ exercises/variable-scope/9.rb
@@ -7,6 +7,10 @@ end
 
 puts a  # will print 7
 
-# This is an example of shadowing - there are two local variables with the same name. While the local variable initialized on line 1 can be accessed from within the block, the a on line 5 of the block is a different variable local to the block. It is that block local variable that is being reassigned by line 5. 
+# This is an example of variable shadowing. Ruby will first look within the block for a variable called 'a'. Because there is both an a local to the block and an a initialized in outer scope, Ruby won't see the outer scope a while within the block.
+
+# The a on line 5 of the block is a different variable local to the block. It is the block's local variable a that is being reassigned by line 5. The outer local variable a is unchanged.
 
 # The #puts call on line 8 will output the value of the a initialized on line 1 - 7.
+
+# To avoid variable shadowing - use unique names! 
