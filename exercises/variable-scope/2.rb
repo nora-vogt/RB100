49d61e495ea8c5cadc2051bbeda8b2b88826a15a
Nora Vogt <noravogt@protonmail.com>
2023-04-14 10:07:27 -0500

    second pass completing exercise

--- exercises/variable-scope/2.rb
+++ exercises/variable-scope/2.rb
@@ -1,14 +1,12 @@
 a = 7
 
-def my_value(a)
-  a += 10
+def my_value(a) # this 'a' is a var local to the my_value method; not the a initialized on line 1
+  a += 10   # variable reassignment
 end
 
-my_value(a)  # The method will return 17
-puts a   # 7
+my_value(a) # method will return 17
+puts a  # will print 7
 
-# puts a will print 7, because the variable 'a' initialized outside of the my_value method is not being mutated by my_value. Instead, my_value is setting a variable local to the method (also called a) that will reference the value 17. 
+# This code will output 7. Despite both variables having the same name of 'a', the variable 'a' initialized in the my_value method is a different variable local to the method. That local variable is being re-assigned on line 4 of the method, not the variable a initialized on line 1. 
 
-# Additionally, methods have self-contained scope. The 'a' initialized on line 1 is not available to re-assign within the method definition of my_value. Likewise, the local variable 'a' within the my_value method is not available outside the method. 
-
-# Also, numbers in Ruby are immutable. There is no way to mutate the value of the variable 'a' initialized on line 1 by the my_value method. 
+# Additionally, numbers are immutable in Ruby. There is no way to mutate the value of the local variable a initialized on line 1 from within the my_value method. 
