8ba659aa55b6c44af3fae96e38cbaba1610a9aba
Nora Vogt <noravogt@protonmail.com>
2023-04-14 10:33:28 -0500

    corrected method return value

--- exercises/variable-scope/4.rb
+++ exercises/variable-scope/4.rb
@@ -4,11 +4,11 @@ def my_value(b)
   b[2] = '-'  # []= method; element assignment, which is destructive 
 end
 
-p my_value(a) # method will return "Xy-zy"
+my_value(a) # method will return "-", because the return value of element assignment is the the assigned value.
 puts a  # prints "Xy-zy"
 
 # The my_value method initializes a local variable 'b'. When invoking the method on line 7 and passing in 'a' as an argument, the variable 'b' local to the method is bound to the same String object that 'a' references.
 
-# On line 4 of the my_value method, a destructive method is invoked - element assignment. Because 'b' points to the same object that 'a' points to, that object itself is mutated by invokign the my_value method. 
+# On line 4 of the my_value method, a destructive method is invoked - element assignment. Because 'b' points to the same object that 'a' points to, that object itself is mutated by invoking the my_value method. 
 
 # Therefore, line 8 will output that mutated String object - "Xy-zy".
