a73d6350ddbbf00bf9387588581ad2be7ed9d4bb
Nora Vogt <noravogt@protonmail.com>
2023-04-14 10:19:48 -0500

    added notes

--- exercises/variable-scope/3.rb
+++ exercises/variable-scope/3.rb
@@ -7,4 +7,6 @@ end
 my_value(a + 5) # method will return 12
 puts a  # prints 7
 
-# Because methods have self-contained scope in Ruby, the local var a initialized on line 1 is not accessible from within the my_value method. The local var a that is initialized on line 4 of the method definition is a different variable. The my_value method initialies a variable local to the method called 'a' to reference the same value that b references. The local var a initialized on line 1 is unchanged, and 7 will still be output by the #puts call on line 8.
+# Because methods have self-contained scope in Ruby, the local var a initialized on line 1 is not accessible from within the my_value method. The local var a that is initialized on line 4 of the method definition is a different variable. The my_value method initialies a variable local to the method called 'a' to reference the same value that b references. This variable is not accessible outside of the method body.
+
+# The local var a initialized on line 1 is unchanged, and 7 will still be output by the #puts call on line 8.
