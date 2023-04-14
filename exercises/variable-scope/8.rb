ada0994e2c508e781bfd422bfdd311fd75e183a0
Nora Vogt <noravogt@protonmail.com>
2023-04-14 11:03:08 -0500

    second pass completing exercise

--- exercises/variable-scope/8.rb
+++ exercises/variable-scope/8.rb
@@ -1,7 +1,9 @@
 array = [1, 2, 3]
 
 array.each do |element|
-  a = element
+  a = element # var local to the block 'a' is initialized, and with each iteration set to the value of element
 end
 
-puts a  # This will raise an error - undefine local var or method 'a'. Variables initalized within the inner scope of a block are not accessible in the outer scope. So, the variable 'a' initialized on line 4 is only available to the block, not to the puts invocation on line 7 (or anywhere else outside the block).
+puts a # will raise an error - nothing will be printed
+
+# The inner scope of a block can access variables initialied in an outer scope, but variables initialied within the block are not accessible outside of the block. The variable 'a' is initialized on line 4 within the block and is not acessible outside of the block, which will cause line 7 to raise an error.
