7afd7ad512e92a481a88d378fa6ec8b36fd7d3b4
Nora Vogt <noravogt@protonmail.com>
2023-04-14 10:17:28 -0500

    second pass completing exercise

--- exercises/variable-scope/3.rb
+++ exercises/variable-scope/3.rb
@@ -1,13 +1,10 @@
 a = 7
 
 def my_value(b)
-  a = b   # initializing local variable a, setting it equal to the value passed in by parameter 'b'
+  a = b   # var assignment; local var a is bound to the same value that b references
 end
 
-my_value(a + 5) # this will return 12.
-puts a  # still will print 7
+my_value(a + 5) # method will return 12
+puts a  # prints 7
 
-# Methods have self-contained scope. The local variable 'a' initialized on line 1 is not available to reassign in the method body of my_value. 
-
-#The 'a' in the method definition is a variable local to the method definition that is being assigned to the value bound to the parameter b - it's a different variable entirely than the 'a' on line 1. Additionally, the local variables within the method definition are not available to access outside the method.
-
+# Because methods have self-contained scope in Ruby, the local var a initialized on line 1 is not accessible from within the my_value method. The local var a that is initialized on line 4 of the method definition is a different variable. The my_value method initialies a variable local to the method called 'a' to reference the same value that b references. The local var a initialized on line 1 is unchanged, and 7 will still be output by the #puts call on line 8.
