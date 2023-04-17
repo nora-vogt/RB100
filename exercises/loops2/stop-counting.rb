e7c01314f7fb327f261cc33074899e0132f7c6c6
Nora Vogt <noravogt@protonmail.com>
2023-04-17 14:34:42 -0500

    added further exploration

--- exercises/loops2/stop-counting.rb
+++ exercises/loops2/stop-counting.rb
@@ -8,5 +8,12 @@ end
   puts index
   break if index == 4
 end
-
 # Five values will be printed
+
+# Now how many values?
+5.times do |index|
+  puts index
+  break if index < 7
+end
+
+# Only one value - 0 will be printed. The index will always be less than 7, so this will break after the first iteration. 
