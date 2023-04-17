7f81d709edee887e7f732cc2b9816c1ed31e39d8
Nora Vogt <noravogt@protonmail.com>
2023-04-17 14:28:31 -0500

    second pass completing exercise

--- exercises/loops2/empty-the-array.rb
+++ exercises/loops2/empty-the-array.rb
@@ -4,12 +4,3 @@ loop do
   puts names.shift
   break if names.empty?
 end
-
-# Can you change this to print the names from last to first?
-names = ['Sally', 'Joe', 'Lisa', 'Henry']
-
-loop do
-  puts names.pop
-  break if names.empty?
-end
-
