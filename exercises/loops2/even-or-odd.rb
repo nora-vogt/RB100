98bdfc1b2a4ad66db2f96c2f583592a8423bea66
Nora Vogt <noravogt@protonmail.com>
2023-04-17 13:26:59 -0500

    second pass completing exercise

--- exercises/loops2/even-or-odd.rb
+++ exercises/loops2/even-or-odd.rb
@@ -1,5 +1,3 @@
-# Write a loop that prints numbers 1-5 and whether the number is even or odd. Use the code below to get started.
-
 count = 1
 
 loop do
@@ -8,7 +6,7 @@ loop do
   else
     puts "#{count} is even!"
   end
-
-  break if count == 5  # Makes sense to check if the count is equal to 5 before incrementing, as opposed to incrementing count to 6 and then checking count > 5 as I originally did.
+  
+  break if count == 5
   count += 1
 end
