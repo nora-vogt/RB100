2bc15ad0d4f735cbadc374c1b665dff5dbabe7c8
Nora Vogt <noravogt@protonmail.com>
2023-04-17 14:15:57 -0500

    added alternate solution

--- exercises/loops2/catch-the-number.rb
+++ exercises/loops2/catch-the-number.rb
@@ -1,5 +1,12 @@
+# loop do
+#   number = rand(100)
+#   puts number
+#   break if (0..10).include?(number)
+# end
+
+# Alternate Solution
 loop do
   number = rand(100)
   puts number
-  break if (0..10).include?(number)
+  break if number.between?(0, 10)
 end
