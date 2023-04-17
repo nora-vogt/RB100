61b30775d2891b176c7705fa22b88dd899c21d3c
Nora Vogt <noravogt@protonmail.com>
2023-04-17 14:25:49 -0500

    second pass completing exercise

--- exercises/loops2/insert-numbers.rb
+++ exercises/loops2/insert-numbers.rb
@@ -2,10 +2,8 @@ numbers = []
 
 loop do
   puts 'Enter any number:'
-  input = gets.chomp.to_i 
-  numbers << input
-
+  input = gets.chomp.to_i
+  numbers << input    # can also use number.push(input)
   break if numbers.size == 5
 end
-
 puts numbers
