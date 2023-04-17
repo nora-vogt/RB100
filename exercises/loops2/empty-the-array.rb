ed72298e859ebb32aa43d91890d2083680ab7ed4
Nora Vogt <noravogt@protonmail.com>
2023-04-17 14:29:42 -0500

    added further exploration

--- exercises/loops2/empty-the-array.rb
+++ exercises/loops2/empty-the-array.rb
@@ -1,6 +1,13 @@
 names = ['Sally', 'Joe', 'Lisa', 'Henry']
 
+# loop do
+#   puts names.shift
+#   break if names.empty?
+# end
+
+# Further Exploration - remove and print names from last to first
+
 loop do
-  puts names.shift
+  puts names.pop
   break if names.empty?
 end
