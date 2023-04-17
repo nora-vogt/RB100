dc6964cf189574088c8a9edf17e6b3b49ad37c9e
Nora Vogt <noravogt@protonmail.com>
2023-04-17 12:21:59 -0500

    second pass completing exercise

--- exercises/loops/control-the-loop.rb
+++ exercises/loops/control-the-loop.rb
@@ -1,15 +1,17 @@
 iterations = 1
 
-loop do
-  puts "Number of iterations = #{iterations}"
-  iterations += 1  # must to increment iterations
-  break if iterations > 5
-end
+# loop do
+#   puts "Number of iterations = #{iterations}"
+#   iterations += 1
+#   break if iterations > 5
+# end
 
-# If the break statement is moved up one line so it runs before iterations is incremented, what would need to be changed?
-    # Nothing! It will still work. But better syntax / makes more sense to change to break if iterations == 5? 
+# Further Exploration: If the break statement is moved up one line so it runs before iterations is incremented, what would need to be changed?
+
+# If the break statement is moved up one line, we would need to change the conditional to break if iterations == 5.
+
 loop do
   puts "Number of iterations = #{iterations}"
   break if iterations == 5
-  iterations += 1  # reassignment. iterations = iterations + 1
+  iterations += 1
 end
