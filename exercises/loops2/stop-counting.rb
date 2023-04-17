4ce115fd3b19c0e0aed93f2e19a97cf6f246ed45
Nora Vogt <noravogt@protonmail.com>
2023-04-17 14:33:35 -0500

    second pass completing exercise

--- exercises/loops2/stop-counting.rb
+++ exercises/loops2/stop-counting.rb
@@ -1,21 +1,12 @@
-# Modify the block so that it prints the current number
-# And stops iterating when the current nummber equals 2
-
 5.times do |index|
   puts index
   break if index == 2
 end
 
-# How many values will be printed from the following code?
-
+# Further Exploration - How many values will be printed?
 5.times do |index|
   puts index
   break if index == 4
 end
-# 5 values will be printed (0, 1, 2, 3, 4)
 
-5.times do |index|
-  puts index
-  break if index < 7
-end
-# 1 value (0) will be printed
+# Five values will be printed
