c68af923053b273c0307f302ef2886d3d7ef4d47
Nora Vogt <noravogt@protonmail.com>
2023-04-18 10:54:55 -0500

    edited further exploration

--- exercises/user-input/your-age-in-months.rb
+++ exercises/user-input/your-age-in-months.rb
@@ -4,4 +4,4 @@ age_in_months = age_in_years * 12
 
 puts "You are #{age_in_months} months old."
 
-# Further Exploration: When a non-numeric value is entered, calling #to_i on the string converts the string to the integer 0.
+# Further Exploration: When a non-numeric value is entered, calling #to_i converts the string to the integer 0.
