6ada1f81fae08caa03e867a2c48b2edab31c87c2
Nora Vogt <noravogt@protonmail.com>
2023-04-18 11:09:01 -0500

    completed exercise

--- exercises/user-input/print-something2.rb
+++ exercises/user-input/print-something2.rb
@@ -1,13 +1,13 @@
 loop do
-  puts '>> Do you want me to print something? (y/n)'
+  puts ">> Do you want me to print something? (y/n)"
   input = gets.chomp.downcase
 
   if input == 'y'
-    puts 'something'
+    puts "something"
     break
   elsif input == 'n'
     break
-  elsif
-    puts '>> Invalid input! Please enter y or n'
+  else 
+    puts ">> Invalid input! Please enter y or n"
   end
 end
