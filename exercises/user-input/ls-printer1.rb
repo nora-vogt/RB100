4e3dcd4fa77799ea5afbcdce1497a9bb6249ce0c
Nora Vogt <noravogt@protonmail.com>
2023-04-18 11:40:26 -0500

    second pass completing exercise

--- exercises/user-input/ls-printer1.rb
+++ exercises/user-input/ls-printer1.rb
@@ -1,11 +1,11 @@
 number_of_lines = nil
-
 loop do
   puts ">> How many output lines do you want? Enter a number >= 3:"
-  number_of_lines = gets.to_i  # don't need #chomp when converting to an Integer
+  number_of_lines = gets.to_i
+
   break if number_of_lines >= 3
 
   puts ">> That's not enough lines."
 end
 
-number_of_lines.times { puts "Launch School is the best!" }
+number_of_lines.times { puts 'Launch School is the best!' }
