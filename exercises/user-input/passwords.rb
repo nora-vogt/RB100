ea5083bb6cb0f7e5ea8abea4862b1de1b1ca9c7c
Nora Vogt <noravogt@protonmail.com>
2023-04-18 11:47:18 -0500

    second pass completing exercise

--- exercises/user-input/passwords.rb
+++ exercises/user-input/passwords.rb
@@ -1,17 +1,11 @@
-# display a welcome message after user enters correct password
-# password is a string defined as a constant
-# keep asking for a password until the user enters the correct password
-
-
-PASSWORD = "DAffodil"
-user_input = nil
+PASSWORD = 'Balsamfir'
 
 loop do
-  puts ">> Please enter your password: "
+  puts ">> Please enter your password:"
   user_input = gets.chomp
-  break if user_input == PASSWORD
 
-  puts ">> Invalid Password!"
+  break if user_input == PASSWORD
+  puts ">> Invalid password!"
 end
 
 puts "Password accepted. Welcome!"
