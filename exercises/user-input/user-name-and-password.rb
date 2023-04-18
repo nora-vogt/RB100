42ecbf07c9d5b8d2ca6d3d8bc10557410e0ca88c
Nora Vogt <noravogt@protonmail.com>
2023-04-18 11:52:24 -0500

    second pass completing exercise

--- exercises/user-input/user-name-and-password.rb
+++ exercises/user-input/user-name-and-password.rb
@@ -1,20 +1,15 @@
-# modify program to require a username
-# program should ask for both username and password first
-# then validate both at same time
-# issue a generic error message is one or both are incorrect; do not specify which is incorrect
-
-USERNAME = "jupiter"
-PASSWORD = "DAffodil"
+PASSWORD = 'Balsamfir'
+USERNAME = 'admin'
 
 loop do
   puts ">> Please enter user name:"
-  username_try = gets.chomp
-  puts ">> Please enter your password: "
-  password_try = gets.chomp
+  user_input_username = gets.chomp
 
-  break if username_try == USERNAME && password_try == PASSWORD
+  puts ">> Please enter your password:"
+  user_input_password = gets.chomp
 
+  break if user_input_password == PASSWORD && user_input_username == USERNAME
   puts ">> Authorization failed!"
 end
 
-puts "You are now logged in. Welcome!"
+puts "You are now logged in. Welcome, #{USERNAME}!"
