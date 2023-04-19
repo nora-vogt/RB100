3b3d9dd1924559567a18010dd993d10d47669fa7
Nora Vogt <noravogt@protonmail.com>
2023-04-19 12:07:34 -0400

    second pass completing exercise

--- exercises/user-input/dividing-numbers.rb
+++ exercises/user-input/dividing-numbers.rb
@@ -1,17 +1,9 @@
-# ask user to enter two integers
-# prints the results of dividing first int by second int
-# second int must not be 0
-# must validate input to be sure it is an integer
-
-  # check if numerator is valid
-    # if not valid, display error and restart loop
-    # if valid, proceed to ask for denominator (second loop)
-  # check if denominator is 0
-      # if true, display error message
-      # loop back to ask for denominator again
-  # if denominator is not 0, check if denominator is valid
-      # if valid, break from loop
-      # otherwise, display general invalid input message
+require 'pry'
+# ask the user to enter two integers
+  # the second integer cannot be zero - if it is, display no zero error message
+  # if either integer fails validation, display "only integers are allowed message"
+# print the result of dividing the first by the second
+# must validate inout to be sure it is an integer
 
 def valid_number?(number_string)
   number_string.to_i.to_s == number_string
@@ -33,8 +25,9 @@ loop do
   puts ">> Please enter the denominator:"
   denominator = gets.chomp
 
-  if denominator == '0'
+  if denominator == "0"
     puts ">> Invalid input. A denominator of 0 is not allowed."
+    next
   elsif valid_number?(denominator)
     break
   else
@@ -42,6 +35,6 @@ loop do
   end
 end
 
-quotient = numerator.to_i / denominator.to_i
+answer = numerator.to_i / denominator.to_i
 
-puts "#{numerator} / #{denominator} is #{quotient}"
+puts ">> #{numerator} / #{denominator} is #{answer}"
