62873b35f41a87114016a8bf16cf104bd82de714
Nora Vogt <noravogt@protonmail.com>
2023-04-20 13:38:54 -0400

    refactored exercise

--- exercises/user-input/opposites-attract.rb
+++ exercises/user-input/opposites-attract.rb
@@ -1,44 +1,25 @@
-# request two integers from user
-  # one integer must be positive
-  # one integer must be negative
-  # however, order does not matter
-# check for pos/neg requirement AFTER both ints are entered
-  # if reqs are not met, start the loop over
-# if pos/neg reqs are met: break from the loop
-
-# outside the loop:
-  # add them together
-#  print result
+require 'pry'
 
 def valid_number?(number_string)
   number_string.to_i.to_s == number_string && number_string.to_i != 0
+end
+
+def get_and_validate_number
+  loop do
+    puts ">> Please enter a positive or negative integer:"
+    user_string = gets.chomp
+    return user_string if valid_number?(user_string)
+
+    puts ">> Invalid input. Only non-zero integers are allowed."
+  end
 end
 
 first_int = nil
 second_int = nil
 
 loop do
-  first_string = ""
-  second_string = ""
-
-  loop do
-    puts ">> Please enter a positive or negative integer:"
-    first_string = gets.chomp
-    break if valid_number?(first_string)
-
-    puts ">> Invalid input. Only non-zero integers are allowed."
-  end
-
-  loop do
-    puts ">> Please enter a positive or negative integer:"
-    second_string = gets.chomp
-    break if valid_number?(second_string)
-
-    puts ">> Invalid input. Only non-zero integers are allowed."
-  end
-
-  first_int = first_string.to_i
-  second_int = second_string.to_i
+  first_int = get_and_validate_number.to_i
+  second_int = get_and_validate_number.to_i
 
   break if (first_int > 0 || second_int > 0 ) && (first_int < 0 || second_int < 0)    # can also use XOR in Ruby: ^
   
@@ -46,8 +27,5 @@ loop do
   puts ">> Please start over."
 end
 
-
 result = first_int + second_int
 puts "#{first_int} + #{second_int} = #{result}"
-
-# turn the nested loops into methods once everything is working
