c2b3a60cd80465f4c05f3091a09d23161ff956ef
Nora Vogt <noravogt@protonmail.com>
2023-04-20 13:43:34 -0400

    refactored exercise

--- exercises/user-input/opposites-attract.rb
+++ exercises/user-input/opposites-attract.rb
@@ -8,7 +8,7 @@ def get_and_validate_number
   loop do
     puts ">> Please enter a positive or negative integer:"
     user_string = gets.chomp
-    return user_string if valid_number?(user_string)
+    return user_string.to_i if valid_number?(user_string)   # use return intead of break when extracting loop code to a method
 
     puts ">> Invalid input. Only non-zero integers are allowed."
   end
@@ -18,10 +18,11 @@ first_int = nil
 second_int = nil
 
 loop do
-  first_int = get_and_validate_number.to_i
-  second_int = get_and_validate_number.to_i
+  first_int = get_and_validate_number
+  second_int = get_and_validate_number
 
-  break if (first_int > 0 || second_int > 0 ) && (first_int < 0 || second_int < 0)    # can also use XOR in Ruby: ^
+  break if first_int * second_int < 0   # can use the product to validate - a negative * a postive will be a negative number
+  # I also like the XOR, this would work: (num1 < 0) ^ (num2 < 0)
   
   puts ">> Sorry. One integer must be positive, one must be negative."
   puts ">> Please start over."
