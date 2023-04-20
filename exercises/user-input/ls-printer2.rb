7ab9d3e0fb0bb05542ca389a13353cc657dea315
Nora Vogt <noravogt@protonmail.com>
2023-04-20 12:38:29 -0400

    second pass completing exercise

--- exercises/user-input/ls-printer2.rb
+++ exercises/user-input/ls-printer2.rb
@@ -1,12 +1,22 @@
+number_of_lines = nil
 loop do
   puts ">> How many output lines do you want? Enter a number >= 3: (Q to quit)"
-  input = gets.chomp.downcase
+  number_of_lines = gets.chomp
 
-  if input == 'q'
+  if number_of_lines.downcase == "q"
     break
-  elsif input.to_i < 3
-    puts ">> That's not enough lines."
   else
-    input.to_i.times { puts "Launch School is the best!" }
+    number_of_lines = number_of_lines.to_i
   end
+
+  if number_of_lines < 3
+    puts ">> That's not enough lines."
+    next
+  end
+  number_of_lines.times { puts 'Launch School is the best!' }
 end
+
+
+
+# program needs to loop after printing specified number of lines
+# program only exits when user enters Q or q (case insensitive)
