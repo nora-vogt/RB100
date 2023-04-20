9e6f5fb8784a657817e2d2301cb1372372d64f1b
Nora Vogt <noravogt@protonmail.com>
2023-04-20 12:40:29 -0400

    refactored exercise

--- exercises/user-input/ls-printer2.rb
+++ exercises/user-input/ls-printer2.rb
@@ -5,18 +5,9 @@ loop do
 
   if number_of_lines.downcase == "q"
     break
-  else
-    number_of_lines = number_of_lines.to_i
-  end
-
-  if number_of_lines < 3
+  elsif number_of_lines.to_i < 3
     puts ">> That's not enough lines."
-    next
+  else 
+    number_of_lines.to_i.times { puts 'Launch School is the best!' }
   end
-  number_of_lines.times { puts 'Launch School is the best!' }
 end
-
-
-
-# program needs to loop after printing specified number of lines
-# program only exits when user enters Q or q (case insensitive)
