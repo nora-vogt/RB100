31cb16f559f2ca6ff54c09dede7a5a898b933f59
Nora Vogt <noravogt@protonmail.com>
2023-04-20 13:28:44 -0400

    second pass completing exercise

--- exercises/user-input/opposites-attract.rb
+++ exercises/user-input/opposites-attract.rb
@@ -1,53 +1,53 @@
-require 'pry'
-# accept two integers from user
-# add them together
-# display the result
+# request two integers from user
+  # one integer must be positive
+  # one integer must be negative
+  # however, order does not matter
+# check for pos/neg requirement AFTER both ints are entered
+  # if reqs are not met, start the loop over
+# if pos/neg reqs are met: break from the loop
 
-# one integer must be positive
-# one integer must be negative
-# the order of pos vs. neg does not matter
-# cannot enter zero
-
-# check for pos/neg after both ints have been entered
-# start over if the requirements are not met
+# outside the loop:
+  # add them together
+#  print result
 
 def valid_number?(number_string)
   number_string.to_i.to_s == number_string && number_string.to_i != 0
 end
 
-int_one = nil
-int_two = nil
+first_int = nil
+second_int = nil
 
 loop do
-
-  string_one = nil
-  string_two = nil
+  first_string = ""
+  second_string = ""
 
   loop do
     puts ">> Please enter a positive or negative integer:"
-    string_one = gets.chomp
-    
-    break if valid_number?(string_one)
+    first_string = gets.chomp
+    break if valid_number?(first_string)
+
     puts ">> Invalid input. Only non-zero integers are allowed."
   end
 
   loop do
     puts ">> Please enter a positive or negative integer:"
-    string_two = gets.chomp
-    
-    break if valid_number?(string_two)
+    second_string = gets.chomp
+    break if valid_number?(second_string)
+
     puts ">> Invalid input. Only non-zero integers are allowed."
   end
 
-  int_one = string_one.to_i
-  int_two = string_two.to_i
+  first_int = first_string.to_i
+  second_int = second_string.to_i
 
-  break if (int_one > 0) ^ (int_two > 0)
-
+  break if (first_int > 0 || second_int > 0 ) && (first_int < 0 || second_int < 0)    # can also use XOR in Ruby: ^
+  
   puts ">> Sorry. One integer must be positive, one must be negative."
   puts ">> Please start over."
 end
 
-result = int_one.to_i + int_two.to_i
 
-puts "#{int_one} + #{int_two} = #{result}"
+result = first_int + second_int
+puts "#{first_int} + #{second_int} = #{result}"
+
+# turn the nested loops into methods once everything is working
