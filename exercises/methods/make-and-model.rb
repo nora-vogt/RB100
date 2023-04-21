676311864b229f3d38e09f016a6b2f4e4f2a3499
Nora Vogt <noravogt@protonmail.com>
2023-04-21 14:33:54 -0400

    second pass completing exercise

--- exercises/methods/make-and-model.rb
+++ exercises/methods/make-and-model.rb
@@ -1,6 +1,10 @@
-# write method 'car'
-  # car takes 2 arguments
-  # prints a string containing the values of both arguments
+# def car(make, model)
+#   puts "#{make} #{model}"
+# end
+
+# car('Toyota', 'Corolla')
+
+# Further Exploration
 
 def car(make, model)
   "#{make} #{model}"
@@ -8,6 +12,4 @@ end
 
 puts car('Toyota', 'Corolla')
 
-# Before modification (with a #puts call in the car method), the car method returned nil, because puts returns nil. 
-
-# Now, the car method will return a string interpolated with whatever values are passed to the parameters make and model ('Toyota Corolla').
+# When #puts is included inside the method, the car method returns nil. Without the #puts, the car method returns a string containing the interpolated values of the variables make and model - ex, "Toyota Corolla".
