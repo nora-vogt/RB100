a36e8f8cf937a4733e69175f96784defd461f033
Nora Vogt <noravogt@protonmail.com>
2023-04-21 19:38:06 -0400

    second pass completing exercise

--- exercises/methods/naming-animals.rb
+++ exercises/methods/naming-animals.rb
@@ -6,9 +6,6 @@ def cat(name)
   return name
 end
 
-puts "The dog's name is #{dog('Spot')}."
-puts "The cat's name is #{cat('Ginger')}."
+puts "The dog's name is #{dog('Spot')}." # This will raise an exception - argument error - the dog method is not expecting any arguments, but is being called with one. Fix by adding a parameter to the dog method definition.
+puts "The cat's name is #{cat('Ginger')}."  # This will also raise an argument error - this method is expecting 1 argument, but being called with none. Fix by passing in an argument when calling the cat method.
 
-# First error: on line 9, the method dog is called with one argument, when the method is expecting zero arguments due to its definition not including parameters. Fix is to add a parameter on line 1.
-
-# Second error: on line 10, the method cat is invoked with no arguments, when the method is expecting one argument due to its definition including one parameter. Fix is to pass in an argument when calling cat on line 10.
