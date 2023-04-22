c3081ba3f0fe39ca8ec0a7c49db98fb495a8e79b
Nora Vogt <noravogt@protonmail.com>
2023-04-22 14:21:03 -0400

    formatted answer

--- exercises/return/counting-sheep2.rb
+++ exercises/return/counting-sheep2.rb
@@ -13,7 +13,6 @@ puts count_sheep
 # 4
 # 10
 
-# First, 0 1 2 3 4 will be printed by calling the count_sheep method on line 8. Within the count_sheep method, the #times method will print the count five times, starting with zero. 
+# First, 0 1 2 3 4 will be printed by calling the count_sheep method on line 8. The #times method call within the count_sheep method will print the count five times, starting with zero. 
 
-# Next, the #puts call on line 8 will print the return value of the count sheep method. The integer 10 on line 5 is the the last evaluated line of the method, which makes 10 the return value of the method that will be printed.
-
+# Next, the #puts call on line 8 will print the return value of the count_sheep method. The integer 10 on line 5 is the the last evaluated line of the method, which makes 10 the return value of the method that will be printed.
