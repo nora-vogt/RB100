d2122471f0f0a444e41ee8373baa09f8c178d1f4
Nora Vogt <noravogt@protonmail.com>
2023-04-22 14:11:57 -0400

    second pass completing exercise

--- exercises/return/counting-sheep1.rb
+++ exercises/return/counting-sheep1.rb
@@ -1,20 +1,22 @@
 def count_sheep
-  5.times do |sheep|   # code from do...end is an argument we're passing to the #times method. the last line being executed in the method is the invocation of the #times method. therefore, count_sheep's return value will be the return value of the #times method.
-    puts sheep
+  5.times do |sheep|
+    puts sheep  # will print the index
   end
 end
 
 puts count_sheep
+#0
+#1
+#1
+#3
+#4
+#5
+
+# First, the count_sheep method will be invoked. Within the #times block there is a call to #puts - this will print the index five times, starting with 0:
 # 0
 # 1
 # 2
 # 3
 # 4
-# 5
 
-
-# puts on line 7 will initially output the result of calling the count_sheep method - where on line 3 the specified number will be printed on each iteration through the #times method. 
-
-# the #times block is the only code in the method, so it's the last line in the method. #times returns the initial integer it is called on (5), making the return value of count_sheep the Integer 5. 
-
-# the #puts on line 7 will then print the return value of the count_sheep method: 5
+# then, #puts on line 7 will print the return value of the count_sheep method, which is 5. the #times block is the last evaluated line of the expression, and the block returns the integer it is called on - 5.
