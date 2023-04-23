f0ffbee3d051547e4d7ac94aac641fa5ff2f43ef
Nora Vogt <noravogt@protonmail.com>
2023-04-23 10:27:09 -0400

    second pass completing exercise

--- exercises/return/tricky-number.rb
+++ exercises/return/tricky-number.rb
@@ -1,9 +1,14 @@
 def tricky_number
-  if true   # this will always evaluate to true, proceeds to line 3:
-    number = 1    # variable assignment returns the value assigned to the variable. this is the last line evaluated in the method, making the value of the assignment (1) the return value for the method.
-  else   # else will not be executed
-    2    # will not be executed
+  if true
+    number = 1
+  else
+    2
   end
 end
 
-puts tricky_number  # puts will output 1, the return value of the tricky_number method
+puts tricky_number
+# The tricky_number method contains a conditional that will always evaluate to true (true is truthy), so the code on line 3 following the if statement will be executed. 
+
+# The variable assignment on line 3 is the last evaluated line of the method. The return value of variable assignment is the value that is assigned (in this case, the integer 1), and this becomes the return value of the tricky_number method.
+
+# The call to #puts on line 9 will print the return value of the tricky_number method - 1.
