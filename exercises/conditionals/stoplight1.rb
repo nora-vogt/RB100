584a604c73893f237589fc0516172f8a3f619a12
Nora Vogt <noravogt@protonmail.com>
2023-04-23 14:44:34 -0400

    second pass completing exercise

--- exercises/conditionals/stoplight1.rb
+++ exercises/conditionals/stoplight1.rb
@@ -1,10 +1,10 @@
 stoplight = ['green', 'yellow', 'red'].sample
 
-case stoplight
-  when 'green'
-    puts "Go!"
-  when 'yellow'
-    puts "Slow down!"
-  else  # don't need to explicitly compare to 'red' with another when; can use else
-    puts "Stop!"
-end
+puts case stoplight
+      when 'green'
+        'Go!'
+      when 'yellow'
+        'Slow down!'
+      when 'red'
+        'Stop!'
+      end
