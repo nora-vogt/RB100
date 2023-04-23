688a54031e7cb5608bccf689a4c97ba50df97927
Nora Vogt <noravogt@protonmail.com>
2023-04-23 12:37:28 -0400

    added notes

--- exercises/conditionals/true-or-false.rb
+++ exercises/conditionals/true-or-false.rb
@@ -1,3 +1,5 @@
 boolean = [true, false].sample
 
 puts boolean ? "I'm true!" : "I'm false!"
+# puts (boolean ? I'm true! : "I'm false!")
+# boolean ? puts("I'm true!") : puts ("I'm false!")  -> however, this isn't best practice. ternary operators should be used to select between two values, not choose between two actions.
