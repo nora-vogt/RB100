a3e645a6f2561f8277b688ff5c409ec82e72272e
Nora Vogt <noravogt@protonmail.com>
2023-04-23 15:05:35 -0400

    second pass completing exercise

--- exercises/conditionals/stoplight3.rb
+++ exercises/conditionals/stoplight3.rb
@@ -1,7 +1,8 @@
 stoplight = ['green', 'yellow', 'red'].sample
 
+
 case stoplight
-when 'green'  then puts 'Go!'   # make sure then statements are aligned
-when 'yellow' then puts 'Slow down!' 
-else               puts 'Stop!'   # else does not need then because no specific condition is provided to else
+when 'green' then puts 'Go!'
+when 'yellow' then puts 'Slow down!'
+when 'red' then puts 'Stop!'
 end
