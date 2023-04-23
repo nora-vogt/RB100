88f27949ca0269f265ef3cdf6e56713a5356820f
Nora Vogt <noravogt@protonmail.com>
2023-04-23 10:50:09 -0400

    second pass completing exercises

--- exercises/conditionals/unpredictable-weather1.rb
+++ exercises/conditionals/unpredictable-weather1.rb
@@ -1,5 +1,3 @@
 sun = ['visible', 'hidden'].sample
 
-if sun == 'visible'
-  puts "The sun is so bright!"
-end
+puts 'The sun is so bright!' if sun == 'visible'
--- exercises/conditionals/unpredictable-weather2.rb
+++ exercises/conditionals/unpredictable-weather2.rb
@@ -1,3 +1,4 @@
 sun = ['visible', 'hidden'].sample
 
 puts "The clouds are blocking the sun!" unless sun == 'visible'
+# unless will execute if the conditional evaluates to false
