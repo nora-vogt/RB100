a16197670a62405140d817a0e7d41c35688097b0
Nora Vogt <noravogt@protonmail.com>
2023-04-23 12:36:11 -0400

    second pass completing exercise

--- exercises/conditionals/true-or-false.rb
+++ exercises/conditionals/true-or-false.rb
@@ -1,8 +1,3 @@
 boolean = [true, false].sample
 
-boolean ? puts("I'm true!") : puts("I'm false!")  
-
-# can also do -
-  # puts boolean ? "I'm true!" : "I'm false!" 
-  # The ternary operator has a higher precedence than the #puts method call. The ternary expression is evaluated before it is passed to the #puts method. 
-  # it's like puts(boolean ? "I'm true!" : "I'm false!")
+puts boolean ? "I'm true!" : "I'm false!"
