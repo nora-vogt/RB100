4e31e21a1bcc2e1dd2ef1a168689e7feb140f5f2
Nora Vogt <noravogt@protonmail.com>
2023-04-23 12:45:18 -0400

    second pass completing exercise

--- exercises/conditionals/truthy-number.rb
+++ exercises/conditionals/truthy-number.rb
@@ -1,9 +1,9 @@
 number = 7
 
 if number
-  puts "My favorite number is #{number}."
-else 
+  puts "My favorite numebr is #{number}."
+else
   puts "I don't have a favorite number."
 end
 
-# This code will print "My favorite number is 7." because in Ruby, everything except false and nil is "truthy" - meaning it evaluates to true. The number 7 is truthy, so the "if number" expression will evaluate to true and execute the code following the statement.
+# This will print "My favorite number is 7". In conditionals, everything except false and nil is truthy - meaning it evaluates as true. On line 3, the variable 'number', which has a value of the integer 7, evaluates as true and the code following the if statement will always be executed.
