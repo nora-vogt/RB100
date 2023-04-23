8a09059e5774256f061d5bfcb4544ca8c732f0f8
Nora Vogt <noravogt@protonmail.com>
2023-04-23 15:02:56 -0400

    second pass completing exercise

--- exercises/conditionals/cool-numbers.rb
+++ exercises/conditionals/cool-numbers.rb
@@ -1,6 +1,6 @@
 number = rand(10)
 
-if number == 5   # this was variable assignment in original code (number = 5), not comparison. var. assignment evaluates to true, so this condition will always evalaute as true. 
+if number == 5  # originally, this code was performing variable reassignment 'number = 5'. the return value of this reassignment 5, which is truthy, which means this expression always evaluates to true and executes the if branch of the code.
   puts '5 is a cool number!'
 else
   puts 'Other numbers are cool too!'
