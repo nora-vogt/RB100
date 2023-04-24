7d733145244f01eb871eda0655d7c0724364cbc4
Nora Vogt <noravogt@protonmail.com>
2023-04-24 14:01:31 -0400

    second pass completing exercises

--- exercises/arrays/more-than-one.rb
+++ exercises/arrays/more-than-one.rb
@@ -1,13 +1,5 @@
 pets = ['cat', 'dog', 'fish', 'lizard']
+my_pets = pets[2..3]
+#my_pets = pets.select {|pet| pet == 'fish' || pet == 'lizard'}
 
-my_pets = pets.select {|pet| pet == 'fish' || pet == 'lizard'}
-
-puts "I have a pet #{my_pets.first} and a pet #{my_pets.last}!"
-
-# Or, using Array#[]
-# my_pets = pets[2..3]
-# puts "I have a pet #{my_pets[0]} and a pet #{my_pets[1]}!"
-
-# Or, Array#[] - ary[start, length] → new_ary or nil
-# my_pets = pets[2, 2]  # returns ['fish', 'lizard']
-# # puts "I have a pet #{my_pets[0]} and a pet #{my_pets[1]}!"
+puts "I have a pet #{my_pets[0]} and a pet #{my_pets[1]}!"
--- exercises/arrays/new-pet.rb
+++ exercises/arrays/new-pet.rb
@@ -1,5 +1,6 @@
 pets = ['cat', 'dog', 'fish', 'lizard']
-
 my_pet = pets[2]
-puts "I have a pet #{my_pet}!"
+# pets.find {|pet| pet == 'fish'} - will select the first element for which the block evaluates to true
+# pets.select {|pet| pet == 'fish'} - will select all elements for which the block evaluates to true
 
+puts "I have a pet #{my_pet}!"
