6f3c6cdd302af33bc5734dc7d186c325afc480d3
Nora Vogt <noravogt@protonmail.com>
2023-04-24 13:16:21 -0400

    second pass completing exercise

--- exercises/strings/combining-names.rb
+++ exercises/strings/combining-names.rb
@@ -1,23 +1,13 @@
 first_name = 'John'
 last_name = 'Doe'
 
-full_name = first_name + ' ' + last_name # using String Concatenation (String#+)
+full_name = "#{first_name} #{last_name}"  # String interpolation
+# full_name = first_name + ' ' + last_name  # String#+ String concatenation
 puts full_name
 
-#Or
-full_name = "#{first_name} #{last_name}"  # using String Interpolation
-puts full_name
+# Further Exploration - two additional ways to combine first and last name
+# puts first_name.concat(' ', last_name)  # String#concat takes multiple arguments; mutates calling string
 
-#Or
-puts full_name = %Q(#{first_name} #{last_name})  # using Percent String
+# puts last_name.prepend(first_name, ' ') # String#prepend takes multiple arguments; mutates the caller
 
-#Or using String#concat
-# full_name = first_name.concat(' ', last_name)   # mutates the caller.
-# puts full_name => "John Doe"
-
-# Or using String#prepend
-# full_name = last_name.prepend(first_name, ' ')   # mutates the caller. 
-# puts full_name = "John Doe"
-
-# Or using String#<< - appends the given object to string. mutates the caller.
-puts full_name = first_name << ' ' << last_name
+puts %Q(#{first_name} #{last_name}) # Using percent String with uppercase Q
