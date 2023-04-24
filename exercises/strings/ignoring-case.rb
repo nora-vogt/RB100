b5ea0d101a0aed7846620c90d615a09db21de8ef
Nora Vogt <noravogt@protonmail.com>
2023-04-24 13:07:00 -0400

    second pass completing exercises

--- exercises/strings/ignoring-case.rb
+++ exercises/strings/ignoring-case.rb
@@ -1,15 +1,11 @@
 name = 'Roger'
+puts name.casecmp?('RoGeR') #=> true
+puts name.casecmp?('DAVE') #=> false
 
-#casecmp? is case-insensitive; returns true, false, or nil if not a string
-puts name.casecmp?('RoGeR') # returns true
-puts name.casecmp?('DAVE')  # returns false
+# Or
+puts name.casecmp('RoGeR') == 0 #=> true
+puts name.casecmp('DAVE') == 0 #=> false
 
-#casecmp is a case-insensitive version of String#<=>, returns -1, 0, 1, or nil
-puts name.casecmp('RoGeR') == 0 
-# name.casecmp('RoGeR') returns 0, strings are equal when case-insensitive
+# String#casecmp is a case-insensitive version of String#<=>. Returns -1, 0, +1 or nil. 
 
-puts name.casecmp('DAVE') == 0
-# name.casecmp('DAVE') returns 1. the case-insensitive value of the calling string (name) is greater than the argument provided ('DAVE').
-
-puts name.casecmp('Zepplin') 
-# name.casecmp('Zepplin') returns -1. the case-insensitive value of the calling string name is less than the argument provided ('Zepplin'). 
+# String#casecmp? returns true if string and comparisons tring are equal after Unicode case folding (case insensitive), false if they are not equal.
